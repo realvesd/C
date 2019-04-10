bd0792eab1b6fbec21a7b2f2603ef018f7fa286f
realvesd <43302742+realvesd@users.noreply.github.com>
2019-04-09 21:28:48 -0300

    Create criarbiblioteca.c

---
+++ criarbiblioteca.c
@@ -0,0 +1,33 @@
+/** strcpy - copia string para outro array char
+
+ strcmp - comparação entre duas strings
+
+* strcat - concatenação de duas strings
+
+* strlen - retorna tamanho da string.*/
+
+
+void Copiar() {
+char *strcpy (char* primeiro, char*segundo);
+int i;
+
+for (i = 0;i < strlen(primeiro); i++) {
+primeiro[i] = segundo[1];
+
+primeiro[i] = '\0';
+
+return primeiro
+}
+
+void Comparar() {
+
+
+
+  
+}
+
+
+
+
+
+}
