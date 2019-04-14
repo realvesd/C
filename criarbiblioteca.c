074baae2c4dbcc207631f767107e6c8736b6c3f5
realvesd <43302742+realvesd@users.noreply.github.com>
2019-04-14 16:04:09 -0300

    Update criarbiblioteca.c

--- criarbiblioteca.c
+++ criarbiblioteca.c
@@ -1,9 +1,6 @@
 /** strcpy - copia string para outro array char
-
  strcmp - comparação entre duas strings
-
 * strcat - concatenação de duas strings
-
 * strlen - retorna tamanho da string.*/
 
 
@@ -19,15 +16,26 @@ primeiro[i] = '\0';
 return primeiro
 }
 
-void Comparar() {
+int comparar(char*string1, char*string2) {
+int i;
 
-
-
-  
+for (i = 0; i < strlrn(string1); i++) {
+  while (strcmp (string1[i],string2[i]) != 0)
+  return 0;
+}  
 }
 
+int concatenar(char*str1, char*str2) {
+  strcat(str1,str2);
+  return strcat;
+}
 
+int tamanho (char*stringzinha) {
+  int i;
 
+  for (i = 0; i < srtlen(stringzinha); i++);
+  return strlen(stringzinha)
+}
 
 
 }
