6d4011d7a5dfd96fd38078df91aa083187320acb
realvesd <43302742+realvesd@users.noreply.github.com>
2019-04-16 13:22:08 -0300

    Update criarbiblioteca.c

--- criarbiblioteca.c
+++ criarbiblioteca.c
@@ -9,7 +9,7 @@ char *strcpy (char* primeiro, char*segundo);
 int i;
 
 for (i = 0;i < strlen(primeiro); i++) {
-primeiro[i] = segundo[1];
+primeiro[i] = segundo[i];
 
 primeiro[i] = '\0';
 
@@ -19,22 +19,31 @@ return primeiro
 int comparar(char*string1, char*string2) {
 int i;
 
-for (i = 0; i < strlrn(string1); i++) {
-  while (strcmp (string1[i],string2[i]) != 0)
-  return 0;
-}  
+for (i = 0; string1[i] == string2[i]; i++) {
+        if (string1[i] == 0) {
+            return 1;
+        }
+    }
+
+    return 2;
 }
 
-int concatenar(char*str1, char*str2) {
-  strcat(str1,str2);
-  return strcat;
+char* concatenar(char*str1[], char*str2[]) {
+int i,j;
+
+for (i = 0; str1[i] != '\0'; i++);
+for (j =0; str2[j] != '\0'; j++){
+str1[i] = str2[j];
+}
+str1[i] = '\0';
+return str1;
+
 }
 
-int tamanho (char*stringzinha) {
-  int i;
-
-  for (i = 0; i < srtlen(stringzinha); i++);
-  return strlen(stringzinha)
+int contador (const char *string) {
+    int i = 0;
+    for (; string[i] != 0; ++i);
+    return i;
 }
 
 
